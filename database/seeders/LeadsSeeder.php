516dc672dc607df8919da32eb5952696c454ea7c
Dulanga Dasanayake <dulanganiroshan721@gmail.com>
2025-11-13 00:14:03 +0530

    leads seeder update for inquiry type data

--- database/seeders/LeadsSeeder.php
+++ database/seeders/LeadsSeeder.php
@@ -9,6 +9,10 @@ class LeadsSeeder extends Seeder
 {
     public function run(): void
     {
+        $inquiryTypes = [
+            'Facebook', 'Instagram', 'Google Form', 'Survey', 'Referral', 'Walk-in', 'Email', 'WhatsApp', 'Other'
+        ];
+
         $leads = [
             [
                 'first_name' => 'Ubaldo',
@@ -24,6 +28,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(5),
                 'preferred_universities' => 'Harvard University, Stanford University',
                 'special_notes' => 'Looking for MBA programs',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Rosalyn',
@@ -39,6 +44,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(1),
                 'preferred_universities' => 'University of California, MIT',
                 'special_notes' => 'Needs scholarship information',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Lia',
@@ -54,6 +60,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(7),
                 'preferred_universities' => 'Cambridge University, Oxford University',
                 'special_notes' => 'Research focused, needs supervisor contact',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Osvaldo',
@@ -69,6 +76,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(10),
                 'preferred_universities' => 'University of Toronto, McGill University',
                 'special_notes' => 'Ready to apply, needs application assistance',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Jeremy',
@@ -84,6 +92,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(3),
                 'preferred_universities' => 'Monash University, University of Sydney',
                 'special_notes' => 'Urgent - needs passport application help',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Marcelle',
@@ -99,6 +108,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(30),
                 'preferred_universities' => 'University of Melbourne, Australian National University',
                 'special_notes' => 'Successfully enrolled in Master of Data Science',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Wilma',
@@ -114,6 +124,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(15),
                 'preferred_universities' => 'University of British Columbia, University of Alberta',
                 'special_notes' => 'Did not meet entry requirements',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Gay',
@@ -129,6 +140,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(20),
                 'preferred_universities' => 'ETH Zurich, University of Zurich',
                 'special_notes' => 'Funding not secured',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Mariana',
@@ -144,6 +156,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(8),
                 'preferred_universities' => 'National University of Singapore, Nanyang Technological University',
                 'special_notes' => 'Waiting for offer letter',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
             [
                 'first_name' => 'Shaina',
@@ -159,6 +172,7 @@ class LeadsSeeder extends Seeder
                 'inquiry_date' => now()->subDays(12),
                 'preferred_universities' => 'University of Hong Kong, Chinese University of Hong Kong',
                 'special_notes' => 'Interview scheduled next week',
+                'inquiry_type' => $inquiryTypes[array_rand($inquiryTypes)],
             ],
         ];
 
